a95047df5af1bc2d925985669f76aceebc70b020
p-misner <priya.misner@tufts.edu>
2020-09-29 00:23:16 -0700

    successfully switched over to onecall

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { openWeatherKey } from "../apiKeys";
-import PlacesAutocomplete, {
-	geocodeByAddress,
-	getLatLng,
-} from "react-places-autocomplete";
+import { geocodeByAddress } from "react-places-autocomplete";
 import Place from "./Place";
 import Input from "./Input";
 import TempChange from "./TempChange";
 import WeatherImage from "./WeatherImage";
 export default function Form() {
-	const [apiContent, setApi] = useState("wait");
+	// const [apiContent, setApi] = useState("wait");
+	const [apiContent2, setApi2] = useState("wait");
 	const [city, setCity] = useState("Reno, NV, USA");
 	const [submitCity, setSubmitCity] = useState("Reno, NV, USA");
 	const [unit, setUnit] = useState("imperial");
-	// const [latlong, setLatLong] = useState(["",""])
+	const [coords, setCoords] = useState({
+		lat: 33.441792,
+		long: -94.037689,
+	});
 
 	let openWeatherCall =
 		"https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -22,12 +23,21 @@ export default function Form() {
 		unit +
 		"&appid=" +
 		openWeatherKey;
+	let oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&appid=${openWeatherKey}`;
+
+	// useEffect(() => {
+	// 	fetch(openWeatherCall)
+	// 		.then((response) => response.json())
+	// 		.then((json) => setApi([json, json.main, json.weather]));
+	// }, [unit, submitCity]);
 
 	useEffect(() => {
-		fetch(openWeatherCall)
+		fetch(oneCall)
 			.then((response) => response.json())
-			.then((json) => setApi([json, json.main, json.weather]));
-	}, [unit, submitCity]);
+			.then((json) =>
+				setApi2([json, json.current, json.current.weather[0]])
+			);
+	}, [unit, coords]);
 
 	function getForecast(e) {
 		e.preventDefault();
@@ -35,16 +45,42 @@ export default function Form() {
 	}
 
 	function handleSelect(e) {
+		geoCode(e);
 		setCity(e);
 		setSubmitCity(e);
 	}
 
-	// const searchOptions = {
-	// 	types: ['(cities)']
-	// }
+	async function geoCode(value) {
+		try {
+			var result = await geocodeByAddress(value);
+			result = Array.from(result)[0].geometry.location;
+			setCoords({ lat: result.lat(), long: result.lng() });
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
 	const length = submitCity.split(",").map((i) => i.trim());
-	const iconimg = Array.from(apiContent[2])[0].icon;
+	// const iconimg = Array.from(apiContent[2])[0].icon;
+	// const iconimg2 = ;
+	// var hold1 = apiContent2[1];
+	// try {
+	// 	console.log(apiContent2[1].weather[0].icon);
+	// } catch (err) {
+	// 	console.log(err);
+	// }
+	/*	<h2>
+				weather conditions:
+				{Array.from(apiContent[2]).map((condition) => {
+					return (
+						<p key={condition.description}>
+							{" "}
+							{condition.main} ({condition.description})
+						</p>
+					);
+				})}
+			</h2>*/
+
 	return (
 		<div className="content">
 			<form onSubmit={getForecast}>
@@ -57,25 +93,13 @@ export default function Form() {
 					city={city}
 					setCity={setCity}
 					handleSelect={handleSelect}
+					coords={coords}
 				/>
 				<TempChange unit={unit} setUnit={setUnit} />
-
-				<button>Submit</button>
 			</form>
-			<h2>current weather: {apiContent[1].temp}</h2>
-			<h2>feels like: {apiContent[1].feels_like}</h2>
-			<h2>
-				weather conditions:
-				{Array.from(apiContent[2]).map((condition) => {
-					return (
-						<p key={condition.description}>
-							{" "}
-							{condition.main} ({condition.description})
-						</p>
-					);
-				})}
-			</h2>
-			<WeatherImage icon={iconimg}></WeatherImage>
+			<h2>current weather: {apiContent2[1].temp}</h2>
+			<h2>weather conditions: {apiContent2[2].main}</h2>
+			<WeatherImage icon={apiContent2[2].icon}></WeatherImage>
 		</div>
 	);
 }
--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,11 @@
-import React from "react";
-import PlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
+import React, { useState } from "react";
+import PlacesAutocomplete from "react-places-autocomplete";
 
 export default function Input(props) {
 	const searchOptions = {
-		types: ['(cities)']
-	}
+		types: ["(cities)"],
+	};
+
 	return (
 		<PlacesAutocomplete
 			value={props.city}
